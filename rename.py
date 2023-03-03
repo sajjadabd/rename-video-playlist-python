8c1911e7160507f96aa268cf454e0c0ead16cdce
sajjad <mynameissajjad@gmail.com>
2023-03-03 17:33:12 +0330

    rename video playlists

---
+++ rename.py
@@ -0,0 +1,44 @@
+import os
+
+__dirname = os. getcwd()
+
+__EXTENSION = '.mp4'
+
+
+print(__dirname)
+
+
+__MAXIMUM_NUMBER = -1;
+
+
+
+
+for count, filename in enumerate(os.listdir(__dirname)) :
+    file_extension = os.path.splitext(filename)
+    if( file_extension[1] == __EXTENSION ) :
+        fileNumber = int(file_extension[0].split('-')[0])
+        if( __MAXIMUM_NUMBER < fileNumber ) :
+            __MAXIMUM_NUMBER = fileNumber
+
+
+
+print( 'MAXIMUM LENGTH : ' , len(str(__MAXIMUM_NUMBER)) )
+
+
+for count, filename in enumerate(os.listdir(__dirname)) :
+    file_extension = os.path.splitext(filename)
+    if( file_extension[1] == __EXTENSION ) :
+        fileNumber = int(file_extension[0].split('-')[0])
+        print(filename)
+        #print('Length : ' , len(file_extension[0]) , file_extension ) 
+        #print('0'*  ( len(str(__MAXIMUM_NUMBER)) - int(len(file_extension[0])) ) , file_extension  )
+        precedence = '0'*  ( len(str(__MAXIMUM_NUMBER)) - int(len(file_extension[0].split('-')[0])) )
+        #print(precedence + file_extension[0] + file_extension[1]);
+        source = filename
+        destination = precedence + file_extension[0] + file_extension[1]
+        os.rename(source, destination)
+
+
+print()
+print()
+#input("Press Any Key To Continue . . .")
